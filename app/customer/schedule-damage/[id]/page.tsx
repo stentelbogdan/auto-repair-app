b2a2580875c92215e534613b5029f7626fb9b892
Bogdan Stentel <bogdan@MBP-Bogdan-2.fritz.box>
2026-06-21 07:34:08 +0200

    update

--- app/customer/schedule-damage/[id]/page.tsx
+++ app/customer/schedule-damage/[id]/page.tsx
@@ -218,8 +218,9 @@ export default function ScheduleDamagePage() {
               </div>
             </div>
 
-            <div className="rounded-[26px] bg-white p-5 text-black">
+            <div className="overflow-hidden rounded-[26px] bg-white p-5 text-black">
               <label className="text-sm font-bold text-black/70">Data</label>
+
               <input
                 type="date"
                 min={minDate}
