22855adbb6b1eaea603c994ce0ec536bf659ad82
Bogdan Stentel <stentel.bogdan@gmail.com>
2026-07-27 06:46:00 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
 import "./globals.css";
-
 import AppNavbar from "./components/AppNavbar";
-import PersistentDashboardCar from "./components/car-3d/PersistentDashboardCar";
 import { AuthProvider } from "@/lib/auth-provider";
 
 const geistSans = Geist({
@@ -24,9 +21,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
       lang="ro"
@@ -35,9 +30,6 @@ export default function RootLayout({
       <body className="bg-black text-white">
         <AuthProvider>
           <AppNavbar />
-
-          <PersistentDashboardCar />
-
           <main className="flex-1 bg-black">{children}</main>
         </AuthProvider>
       </body>
