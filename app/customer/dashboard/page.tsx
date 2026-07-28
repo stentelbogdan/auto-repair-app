c7b5d4449a827e6db494c919af3355e7fbeba097
Bogdan Stentel <stentel.bogdan@gmail.com>
2026-07-28 19:27:01 +0200

    update

--- app/customer/dashboard/page.tsx
+++ app/customer/dashboard/page.tsx
@@ -176,7 +176,7 @@ function CustomerDashboardContent() {
               />
             </section>
 
-            <section className="relative z-10 mt-auto grid shrink-0 grid-cols-2 gap-3 pt-8 md:mx-auto md:w-full md:max-w-3xl md:gap-6">
+            <section className="relative z-10 mt-auto grid shrink-0 grid-cols-2 gap-3 pt-2 md:mx-auto md:w-full md:max-w-3xl md:gap-6">
               <Card
                 title="Postează daună"
                 desc="Estetică sau mecanică"
